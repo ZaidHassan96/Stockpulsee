5cfc077fab1b7c1d45c9b137ee193b7171368a31
ziani96 <81708016+ziani96@users.noreply.github.com>
2025-03-10 17:32:45 +0000

    Project structure
    created a db file which contains a data folder and a seed folder.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Stockpulsee
+
+Data Cleaning & Analysis Project
+Overview
+This project focuses on data cleaning, transformation, and basic analysis using Pandas, NumPy, and Matplotlib. The dataset comes from <source> and will be processed to extract meaningful insights.
---
+++ db/data/download_data.py
@@ -0,0 +1,61 @@
+import yfinance as yf
+import pandas as pd
+
+
+# # list of tuples containing company symbols and names.
+stocks = [
+    ("AAPL", "Apple Inc."),
+    ("MSFT", "Microsoft Corp."),
+    ("GOOGL", "Alphabet Inc (Google)"),
+    ("GOOG", "Alphabet Inc (Google) - Class C"),
+    ("AMZN", "Amazon.com Inc"),
+    ("NVDA", "NVIDIA Corporation"),
+    ("META", "Meta Platforms Inc(Facebook)"),
+    ("TSLA", "Tesla Inc"),
+    ("TSM", "Taiwan Semiconductor Manufacturing Company (TSMC)"),
+    ("BRK-B", "Berkshire Hathaway Inc. - Class B"),
+    ("ADBE", "Adobe Inc."),
+    ("INTC", "Intel Corporation"),
+    ("ASML", "ASML Holding"),
+    ("ORCL", "Oracle Corporation"),
+    ("CRM", "Salesforce Inc."),
+    ("AMD", "Advanced Micro Devices"),
+    ("QCOM", "Qualcomm Incorporated"),
+    ("PYPL", "PayPal Holdings Inc."),
+    ("SPOT", "Spotify Technology S.A."),
+    ("ZM", "Zoom Video Communications"),
+    ("NOW", "ServiceNow Inc.")
+]
+
+
+
+stock_list_data = []
+
+# Looping through the list and downloading each companies data by using the yfinance.download function which uses the symbol i.e "AMZN" for the company Amazon.
+for symbol, name in stocks:
+    data = yf.download(symbol, period="1y")
+    data["Company"] = name
+    data.reset_index(inplace=True)
+    data.columns = data.columns.droplevel(1)
+    data.columns.name = None
+    stock_list_data.append(data)
+
+
+
+
+
+stock_list_data = pd.concat(stock_list_data)
+
+
+
+
+
+stock_list_data['Daily_Return'] = (((stock_list_data['Close'] - stock_list_data['Open']) / stock_list_data['Open']) * 100)
+
+stock_list_data['SMA_200'] = stock_list_data.groupby("Company")['Close'].transform(lambda x: x.rolling(window=200).mean())
+
+stock_list_data['Volatility'] = stock_list_data.groupby("Company")['Daily_Return'].transform(lambda x: x.rolling(window=50).std())
+
+stock_list_data['Daily_Return'] = stock_list_data['Daily_Return'].round(2).astype(str) + '%'
+
+stock_list_data.round(2).to_csv("stocks.csv", index=False)
